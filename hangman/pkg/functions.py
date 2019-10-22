37c79249831b93752cd7efa455b134c52eaccb13
Wesley Vanitou <iamdemsugar@gmail.com>
2019-10-22 12:45:18 -0400

    Creation of the scoreboard function

--- hangman/pkg/functions.py
+++ hangman/pkg/functions.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from .tools import symbols
+import pickle
+
 # Check if the nickname is valid
 def checker():
     """ The program will check if there aren't any special
@@ -13,9 +15,27 @@ characters in the name of the users."""
                 y += x + ','
                 continue
         if y:
-            print(f"Symbols {y} are not allowed".isupper())
+            print(f"Symbols {y} are not allowed".upper())
         else: 
             return username
+
+def scoreboard(username):
+    """The function will check if the scoreboard exist, if not,
+create a new one and add the user into it."""
+    try:
+        with open('scores', 'rb') as F:
+            S = pickle.load(F)
+            if username in list(S.keys()):
+                print(f"Welcome back {username}")
+            else:
+                S[username] = 0
+                print(f"Welcome {username}")
+    except:
+        with open('scores', 'wb') as F:
+            S = {username: 0}
+            pickle.dump(S, F)
+            print(f"Welcome {username}")
+
 #def match(letter, word):
 #   """The function will take the word, mask it then analyze if
 #the user find the right letter then display them."""
